031aee826a0562f269fe11446d54350292b19118
nadika <nadikazavodovska@gmail.com>
2025-01-29 18:33:05 +0000

    Add forgot password link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,42 +3,44 @@ import mentorMeet from "../assets/images/online-mentor-meeting.jpg";
 
 function Login() {
   return (
-      <div id='login-page'>
-          <div className='login-page_form-block'>
+      <div id="login-page">
+          <div className="login-page_form-block">
               <h1>Sign in with email</h1>
               <p>
                   Connect with experienced mentors who can guide you toward success. Explore
                   detailed reviews and feedback from other mentees to make the best choice. Start
                   your journey today!
               </p>
-              <form action='#' className='login-page_form'>
-                  <label htmlFor='email' className='form-label'>
+              <form action="#" className="login-page_form">
+                  <label htmlFor="email" className="form-label">
                       Email
                   </label>
                   <input
-                      type='email'
-                      id='email'
-                      name='email'
-                      className='form-field-styles form-field'
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="form-field-styles form-field"
                       required
                   />
-                  <label htmlFor='password' className='form-label'>
+                  <label htmlFor="password" className="form-label">
                       Password
                   </label>
                   <input
-                      type='password'
-                      id='password'
-                      name='password'
-                      className='form-field-styles form-field'
+                      type="password"
+                      id="password"
+                      name="password"
+                      className="form-field-styles form-field"
                       required
                   />
-                  <button type='submit' className='form-field-styles form-submit'>
+                  <a className="forgot-pass-link"  href="#">Forgot password</a>
+                  
+                  <button type="submit" className="form-field-styles form-submit">
                       Login
                   </button>
               </form>
           </div>
-          <div className='login-page_img-block'>
-              <img className="login-page_img-block_img" src={mentorMeet} alt='Mentor meeting' />
+          <div className="login-page_img-block">
+              <img className="login-page_img-block_img" src={mentorMeet} alt="Mentor meeting" />
           </div>
       </div>
   );
