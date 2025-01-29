7329d6e95309f86ea5c070125eebca647e4bbe7d
nadika <nadikazavodovska@gmail.com>
2025-01-29 16:54:47 +0000

    add reset.css, add styles

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,37 @@
 import '../css/Login.css';
 
 function Login() {
-  return <div id="login-page">
-    <div id="login-page_form">
-        <h1>Sign in with email</h1>
-    </div>
-  </div>;
+  return (
+      <div id='login-page'>
+          <div className='login-page_form-block'>
+              <h1>Sign in with email</h1>
+              <p>
+                  Connect with experienced mentors who can guide you toward success. Explore
+                  detailed reviews and feedback from other mentees to make the best choice. Start
+                  your journey today!
+              </p>
+              <form action='#' className='login-page_form'>
+                  <label htmlFor='email'>Email</label>
+                  <input
+                      type='email'
+                      id='email'
+                      name='email'
+                      className='login-page_form-field'
+                      required
+                  />
+                  <label htmlFor='password'>Password</label>
+                  <input
+                      type='password'
+                      id='password'
+                      name='password'
+                      className='login-page_form-field'
+                      required
+                  />
+                  <button type='submit' className='login-page_form-submit'>Login</button>
+              </form>
+          </div>
+      </div>
+  );
 }
 
 export default Login;
