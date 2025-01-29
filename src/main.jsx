0cedd27af4acc0b0016d3445b69039088c1b3f21
nadika <nadikazavodovska@gmail.com>
2025-01-29 17:01:55 +0000

    Add font

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import "./css/reset.css";
 import './css/index.css';
-import './css/reset.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
