6de9fd15420e0f52041bd0c659923b564c058d57
nadika <nadikazavodovska@gmail.com>
2025-01-30 16:01:30 +0000

    Change favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./css/App.css";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/profile" element={<Profile />} />                
             </Routes>
         </>
     );
